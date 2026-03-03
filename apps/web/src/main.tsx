5df9ef2cd025a8be899a7e63d04b1f2d409704d0
JobSwipe Stabilization <stabilization@jobswipe.com>
2026-03-03 12:26:43 -0700

    Fix ErrorBoundary import path

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
-import { ErrorBoundary } from "./components/ui/ErrorBoundary";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 import { AppProvider } from "./context/AppContext";
 import { AuthProvider } from "./context/AuthContext";
 import { validateConfig } from "./config";
